912b739a676c414a5fe5ec11ac25a323866bb9f9
MrFranks89 <federico.franchi89@gmail.com>
2024-11-09 12:30:06 +0100

    Add: search bar (bug $keyword)
    Problemi con inserimento di<input name="keyword" value="${keyword}" class="form-control" type="search" placeholder="Search"aria-label="Search"> che ha causato problemi al pom, modificato per far funzionare l'applicazione, che spaccava del tutto

--- src/main/java/it/lessons/pizzeria/controllers/PizzaController.java
+++ src/main/java/it/lessons/pizzeria/controllers/PizzaController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import it.lessons.pizzeria.model.Pizza;
 import it.lessons.pizzeria.repository.PizzaRepository;
@@ -21,8 +22,17 @@ public class PizzaController {
 	private PizzaRepository pizzaRepo;
 
 	@GetMapping
-	public String index(Model model) {
+	public String index(Model model, @RequestParam(name = "keyword", required = false) String keyword) {
+		
 		List<Pizza> allPizze = pizzaRepo.findAll();
+		
+		if(keyword !=null && !keyword.isBlank()) {
+			allPizze = pizzaRepo.findByNomeContaining(keyword);
+			model.addAttribute("keywor", keyword);
+		} else {
+			allPizze = pizzaRepo.findAll();
+		}
+		
 
 		model.addAttribute("pizze", allPizze);
 
